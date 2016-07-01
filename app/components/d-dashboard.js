a6a6da0e4862a2ecc0300ac2c6e36aa3bd9cda2a
Felix Rieseberg <felix@felixrieseberg.com>
2016-07-01 12:16:35 +0200

    :wrench: Add Ember-Electron

--- app/components/d-dashboard.js
+++ app/components/d-dashboard.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  init() {
+    this._super();
 
+    if (window.process) {
+      this.set('isElectron', true);
+    }
+  }
 });
